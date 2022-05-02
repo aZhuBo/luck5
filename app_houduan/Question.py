f1fb40b3e755494ff54f66aac5ad52eb95563ec1
ZhuBo <241900086@qq.com>
2022-05-02 16:01:00 +0800

    优化后端抽题速度

--- app_houduan/Question.py
+++ app_houduan/Question.py
@@ -1,7 +1,7 @@
 import pymongo
 import random
 from pymongo import MongoClient
-host = '127.0.0.1'
+host = '81.70.23.51'
 client = MongoClient(host, 5429)
 db = client.admin
 #user_client = pymongo.MongoClient(host='127.0.0.1', port=5429)
@@ -19,19 +19,33 @@ def questionGet(unit):
     # 随机选取5个题,存到questionList里面
     questionList = []
     questionCnt = 0
-    while questionCnt < 5:
-        randomNum = random.randrange(count) + 1
-        # 查找的随机id
-        result = collection.find_one({'id': str(randomNum)})
-        obunit = result['unit']
-        if unit == '':
-            if randomNum in questionList:
-                continue
-        else:
-            if randomNum in questionList or obunit != unit:
-                continue
-        questionList.append(randomNum)
-        questionCnt += 1
+    objective_unit = { "unit": unit }
+    resultww = collection.find(objective_unit)
+    mubiao_list=[]
+    mubiao_list_id=[]
+    for aa in resultww:
+        mubiao_list.append(aa)
+
+        mubiao_list_id.append(aa['id'])
+    random.shuffle(mubiao_list_id)
+
+    questionList=mubiao_list_id[:5]
+
+    # while questionCnt < 5:
+    #     randomNum = random.randrange(count) + 1
+    #     # 查找的随机id
+    #     result = collection.find_one({"unit": unit})
+    #
+    #     obunit = result['unit']
+    #
+    #     if unit == '':
+    #         if randomNum in questionList:
+    #             continue
+    #     else:
+    #         if randomNum in questionList or obunit != unit:
+    #             continue
+    #     questionList.append(randomNum)
+    #     questionCnt += 1
 
     # 构建题目列表
     titleList = []
@@ -40,4 +54,24 @@ def questionGet(unit):
 
     return titleList
 
+def questionGet_sum():
+    collection = db.questions
+    # 获取数据库中总题目数量
+    count = collection.estimated_document_count()
+    # 随机选取5个题,存到questionList里面
+    questionList = []
+    questionCnt = 0
+    while questionCnt < 5:
+        randomNum = random.randrange(count) + 1
 
+        # 查找的随机id
+        result = collection.find_one({'id': str(randomNum)})
+        if randomNum in questionList :
+            continue
+        questionList.append(randomNum)
+        questionCnt += 1
+    # 构建题目列表
+    titleList = []
+    for i in range(5):
+        titleList.append(collection.find_one({'id': str(questionList[i])}))
+    return titleList
